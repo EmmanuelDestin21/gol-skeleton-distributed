e3e5bfe31631aea5f2f0a919d232920ec5203ab1
Emmanuel Destin <113697335+EmmanuelDestin21@users.noreply.github.com>
2024-10-31 15:55:00 +0000

    Update distributor.go

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,5 +1,11 @@
 package gol
 
+import (
+	"strconv"
+
+	"uk.ac.bris.cs/gameoflife/util"
+)
+
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
@@ -11,15 +17,37 @@ type distributorChannels struct {
 
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
+	// Create a 2D slice to store the world.
+	world := make([][]byte, p.ImageHeight)
+	for i := range world {
+		world[i] = make([]byte, p.ImageWidth)
+	}
 
-	// TODO: Create a 2D slice to store the world.
+	// Request the filename and read the image.
+	filename := strconv.Itoa(p.ImageWidth) + "x" + strconv.Itoa(p.ImageHeight)
+	c.ioCommand <- ioInput
+	c.ioFilename <- filename
+
+	// Populate the world array from the input.
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			world[y][x] = <-c.ioInput
+		}
+	}
 
 	turn := 0
 	c.events <- StateChange{turn, Executing}
 
-	// TODO: Execute all turns of the Game of Life.
+	// Execute all turns of the Game of Life.
+	for turn < p.Turns {
+		world = calculateNextState(p, world)
+		turn++
+	}
+	aliveCells := calculateAliveCells(p, world)
 
-	// TODO: Report the final state using FinalTurnCompleteEvent.
+	// Report the final state using FinalTurnCompleteEvent.
+	FinalTurnCompleteEvent := FinalTurnComplete{turn, aliveCells}
+	c.events <- FinalTurnCompleteEvent
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
@@ -29,4 +57,61 @@ func distributor(p Params, c distributorChannels) {
 
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
+}
+
+func calculateNextState(p Params, world [][]byte) [][]byte {
+	newWorld := make([][]byte, p.ImageHeight)
+	for i := range newWorld {
+		newWorld[i] = make([]byte, p.ImageWidth)
+	}
+
+	IMHT := p.ImageHeight
+	IMWD := p.ImageWidth
+
+	for y := 0; y < IMHT; y++ {
+		for x := 0; x < IMWD; x++ {
+			// Calculate sum of 8 neighbors
+			sum := int(world[(y+IMHT-1)%IMHT][(x+IMWD-1)%IMWD]/255) +
+				int(world[(y+IMHT-1)%IMHT][(x+IMWD)%IMWD]/255) +
+				int(world[(y+IMHT-1)%IMHT][(x+IMWD+1)%IMWD]/255) +
+				int(world[(y+IMHT)%IMHT][(x+IMWD-1)%IMWD]/255) +
+				int(world[(y+IMHT)%IMHT][(x+IMWD+1)%IMWD]/255) +
+				int(world[(y+IMHT+1)%IMHT][(x+IMWD-1)%IMWD]/255) +
+				int(world[(y+IMHT+1)%IMHT][(x+IMWD)%IMWD]/255) +
+				int(world[(y+IMHT+1)%IMHT][(x+IMWD+1)%IMWD]/255)
+
+			if world[y][x] == 255 {
+				// Cell is alive
+				if sum < 2 || sum > 3 {
+					newWorld[y][x] = 0 // Underpopulation or overpopulation: cell dies
+				} else {
+					newWorld[y][x] = 255 // Cell survives
+				}
+			} else {
+				// Cell is dead
+				if sum == 3 {
+					newWorld[y][x] = 255 // Reproduction: cell becomes alive
+				} else {
+					newWorld[y][x] = 0 // Cell remains dead
+				}
+			}
+		}
+	}
+	return newWorld
+}
+
+func calculateAliveCells(p Params, world [][]byte) []util.Cell {
+	var aliveCells []util.Cell
+	IMHT := p.ImageHeight
+	IMWD := p.ImageWidth
+
+	for y := 0; y < IMHT; y++ {
+		for x := 0; x < IMWD; x++ {
+			if world[y][x] == 255 {
+				aliveCells = append(aliveCells, util.Cell{X: x, Y: y})
+			}
+		}
+	}
+
+	return aliveCells
 }
